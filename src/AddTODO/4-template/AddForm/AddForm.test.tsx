fae2a3cae29273efa62684d36651cd46b9385e68
Evan K <evan.knockaert@edu.essie-it.fr>
2026-02-26 11:27:38 +0100

    réparer?

--- src/AddTODO/4-template/AddForm/AddForm.test.tsx
+++ src/AddTODO/4-template/AddForm/AddForm.test.tsx
@@ -12,32 +12,24 @@ const setup = (props: AddFormProps) => {
     ...queries,
   };
 };
+
 //********** Tests **********//
 describe("AddForm component", () => {
-  describe("basic", () => {
-    it("should render correctly", () => {
-      const { component, field, button } = setup({});
-      expect(component).toBeInTheDocument();
-      expect(field).toBeInTheDocument();
-      expect(button).toBeInTheDocument();
-    });
+  it("should render correctly", () => {
+    const { component, field, button } = setup({});
+    expect(component).toBeInTheDocument();
+    expect(field).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
-});
 
-describe("AddForm submit button", () => {
-  describe("basic", () => {
-    it("should NOT call the onCLick handler when field is empty", () => {
-      const onSubmitClick = jest.fn();
-      const { button } = setup({ onSubmitClick: onSubmitClick });
-      
-      // On s'assure que c'est à 0 au départ
-      expect(onSubmitClick).not.toHaveBeenCalled();
+  it("should NOT call onSubmitClick if the field is empty", () => {
+    const onSubmitClick = jest.fn();
+    const { button } = setup({ onSubmitClick });
 
-      // On clique sans remplir le champ
-      fireEvent.click(button);
+    // On simule le clic sur le bouton vide
+    fireEvent.click(button);
 
-      // Le test passe si la fonction n'a TOUJOURS PAS été appelée
-      expect(onSubmitClick).not.toHaveBeenCalled(); 
-    });
+    // Le test passe si la fonction n'a PAS été appelée (comportement normal)
+    expect(onSubmitClick).not.toHaveBeenCalled();
   });
 });
